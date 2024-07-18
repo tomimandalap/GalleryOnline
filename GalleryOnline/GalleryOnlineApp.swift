00f653a3bb8b676827182ea6b7550948c00123c2
tomiputra <tomi.mandala@privyid.tech>
2024-07-18 14:30:51 +0700

    Initial Commit

---
+++ GalleryOnline/GalleryOnlineApp.swift
@@ -0,0 +1,17 @@
+//
+//  GalleryOnlineApp.swift
+//  GalleryOnline
+//
+//  Created by Tomi Mandala Putra on 18/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct GalleryOnlineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
